85b78f11730b8ea2195bfe8f92e6a8de004bccc8
Kegan Hollern <keganhollern@gmail.com>
2026-01-16 20:12:56 -0600

    some final cleanup

--- ai/main.py
+++ ai/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from ag_ui_adk import ADKAgent, add_adk_fastapi_endpoint
 from google.adk.agents import LlmAgent
+from google.adk.planners import BuiltInPlanner
+from google.genai import types
 
 agent = LlmAgent(
     name="assistant",
-    model="gemini-2.5-flash",
-    instruction="Be helpful and fun!"
+    # model="gemini-2.5-flash",
+    model="gemini-3-flash-preview",
+    instruction="Be helpful and fun!",
+    planner=BuiltInPlanner(
+        thinking_config=types.ThinkingConfig(
+            # includes summarized thoughts as outputs
+            # doesn't work well w/ ag-ui and copilotkit
+            include_thoughts=False, 
+            thinking_level=types.ThinkingLevel.MINIMAL
+            # thinking_budget=1024,   
+        )
+    )
 )
 
 adk_agent = ADKAgent(
